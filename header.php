8e06ca17a7a9dc8d475d9fa2888fe0e29fa1ddd9
Emersonssti <emerson.ssti@gmail.com>
2020-11-07 16:04:53 -0300

    Update header.php

--- header.php
+++ header.php
@@ -19,16 +19,6 @@
 
 <body>
 
-	<div class="preloader-wrapper" id="preloader">
-        <div class="motion-line dark-big"></div>
-        <div class="motion-line yellow-big"></div>
-        <div class="motion-line dark-small"></div>
-        <div class="motion-line yellow-normal"></div>
-        <div class="motion-line yellow-small1"></div>
-        <div class="motion-line yellow-small2"></div>
-    </div>
-
-    
 
     <!--MAIN MENU WRAP BEGIN-->
 <div class="main-menu-wrap sticky-menu">
